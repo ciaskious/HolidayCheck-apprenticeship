58def7dc92ee656b5cf9e1a50419dbe74b5baa47
Anastasia Kaloudi-Siskou <kaloudi-siskou@unternehmertum.de>
2018-08-07 17:25:05 +0200

    added functions that check when player X is the winner and wait for user input to restart the game

--- main.py
+++ main.py
@@ -17,11 +17,41 @@ class MyBoard():
         print("---------------")
         print(" %s   | %s | %s " % (self.board[6], self.board[7], self.board[8]))
 
+    # Reset the game
+
+    def replay(self):
+        self.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
     # Puts the new value to the cell. If a move is repeated, the value is not replaced and the player loses his turn.
 
     def update_cell(self, position, player):
         if self.board[position] == " ":
             self.board[position] = player
+
+    # Get all moves a player has done so that we can find the winner
+
+    def get_moves(self, player):
+        moves = []
+        for i in range(0, len(self.board)):
+            if self.board[i] == player:
+                moves.append(i)
+        return moves
+
+    # Search for a winning combination of moves on the board
+
+    def check_win(self, player):
+        winning_combos = ([0, 1, 2], [3, 4, 5], [6, 7, 8],
+                          [0, 3, 6], [1, 4, 7], [2, 5, 8],
+                          [0, 4, 8], [2, 4, 6])
+
+        positions = self.get_moves(player)
+        for combo in winning_combos:
+            win = True
+            for pos in combo:
+                if pos not in positions:
+                    win = False
+            if win:
+                return player
 
 
 if __name__ == "__main__":
@@ -42,6 +72,20 @@ if __name__ == "__main__":
         board.update_cell(x_move-1, "X")
 
         refresh_screen()
+
+        if board.check_win("X"):
+            print("\n=Hurray! You win <3 \n")
+
+            play_again = raw_input("Would you like to play again? (y/N) > ")
+
+            while play_again not in ("y", "N"):
+                play_again = raw_input("Oops! Didn't get that... Would you like to play again? (y/N) > ")
+
+            if play_again == "y":
+                board.replay()
+                continue
+            else:
+                break
 
         print("Computer choosing move...")
         o_move = int(random.randrange(9))
